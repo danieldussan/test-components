2921188f4eaead6afccf48c0bba1427fd282cabb
Daniel Dussan <daniel2014dussan@gmail.com>
2024-09-27 15:08:23 -0500

    Update google analytics config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,9 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(),
-    partytown({
-    config: {
-      forward: ["dataLayer.push"],
-    }}),]
+  ]
 });
